b8888f5b97a4c0e56649f95ab54765081854300b
Alois Napitalai <anapitalai@gmail.com>
2019-05-02 13:45:04 +1000

    first ever

---
+++ assets/js/modal.js
@@ -0,0 +1,6 @@
+var modalHandle = document.getElementById('modal-form');
+var modalButton= document.getElementById('modal-button');
+
+modalButton.onclick = function(){
+    modalHandle.style.display='block';
+}
